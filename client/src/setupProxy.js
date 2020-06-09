21b4a00870f8f68ebfb25299b4b9cba33a907c38
Ryan Lee <ryanm.lee11@gmail.com>
2020-06-09 11:00:55 -0400

    Access Control Allow Origin

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            // target: 'http://localhost:5000',
+            target: 'http://localhost:5000',
             // target: 'https://warm-garden-11318.herokuapp.com', 
             changeOrigin: true,
         })
--- server/index.js
+++ server/index.js
@@ -47,6 +47,14 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(logger('dev'));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 app.use('/api/users', require('./routes/users'));
 app.use("/api/favorite", require("./routes/favorite"));
 
