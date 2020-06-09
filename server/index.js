e2bada16e630b667abb263fb50c15f12d2dc5637
Ryan Lee <ryanm.lee11@gmail.com>
2020-06-09 13:00:31 -0400

    Update

--- server/index.js
+++ server/index.js
@@ -47,11 +47,19 @@ app.use(cookieParser());
 app.use(logger('dev'));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
+  res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
+});
+
+app.get('/', function(req, res, next) {
+  // Handle the get for this route
+});
+
+app.post('/', function(req, res, next) {
+ // Handle the post for this route
 });
 
 app.use('/api/users', require('./routes/users'));
