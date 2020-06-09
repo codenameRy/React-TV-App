4d0f3c29a56e04a5ccd73f6cb37936be141c9745
Ryan Lee <ryanm.lee11@gmail.com>
2020-06-09 10:33:24 -0400

    Redirect folder update

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            // target: 'http://localhost:5000/',
-            target: 'https://warm-garden-11318.herokuapp.com/', 
+            // target: 'http://localhost:5000',
+            target: 'https://warm-garden-11318.herokuapp.com', 
             changeOrigin: true,
         })
     );
