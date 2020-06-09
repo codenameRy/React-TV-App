c842aa99664ebe8cfb7195fab499c6e6b1de3177
Ryan Lee <ryanm.lee11@gmail.com>
2020-06-09 13:17:13 -0400

    update

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ app.use(cookieParser());
 app.use(logger('dev'));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", 'https://warm-garden-11318.herokuapp.com');
+  res.header("Access-Control-Allow-Origin", 'https://vigorous-carson-99c193.netlify.app');
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
